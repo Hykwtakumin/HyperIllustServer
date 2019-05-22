c298e3cfe6467c7cadc1f448f478cc4c74638f15
hykwtakumin <hykwtakumin@gmail.com>
2019-05-23 08:55:21 +0900

    minimum express app

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,65 +1,15 @@
 import * as express from "express";
-import { ErrorRequestHandler, Express } from "express";
-import { Server } from "http";
-import * as helmet from "helmet";
-import { hicRoutes } from "./routes/routes";
-import * as bodyParser from "body-parser";
-import { DocumentNotFoundError } from "./db/errors";
-import * as AWS from "aws-sdk";
-import * as multer from "multer";
-import * as path from "path";
-import * as fs from "fs";
-
-// import {
-//   OverdueError,
-//   ParameterTypeError,
-//   PermissionDeniedError
-// } from "./errors";
-
-import { MongoError } from "mongodb";
-import { apiRoutes } from "./routes/api";
-import * as cookieParser from "cookie-parser";
-
-export function wrap<T>(asyncf: (req, res, next) => Promise<T>) {
-  return (req, res, next) => asyncf(req, res, next).catch(next);
-}
-
-// const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-//   error(err);
-//   let status = 500;
-//   switch (err.constructor) {
-//     case SyntaxError:
-//     case ParameterTypeError:
-//     case OverdueError:
-//     case MongoError:
-//       status = 400;
-//       break;
-//     case PermissionDeniedError:
-//       status = 403;
-//       break;
-//     case DocumentNotFoundError:
-//       status = 404;
-//       break;
-//     default:
-//       return next(err);
-//   }
-
-//   res.status(status).json({ error: err.name, message: err.message });
-// };
+import { configuredS3 } from "./aws";
 
 const app = express();
+app.listen(process.env.PORT || 3000);
 
-const upload = multer({ dest: path.resolve("./public/tmp") });
+app.get("/", (req, res) => {
+  res.json({ message: "hello!" });
+});
 
-export const upload2s3 = async (hicId: string, file: any): Promise<string> => {
-  return new Promise<string>((resolve, reject) => {
-    fs.readFile(file.path, (err, data) => {
-      if (err) throw err;
-    });
-  });
-};
-
-app.post("/upload/:hicId", upload.single("file"), (req, res) => {
+app.post("/upload/:hicId", (req, res) => {
   const hicId: string = req.params.hicId;
-  const mime = req.file.mimetype || "image/svg+xml";
+  const mime: string = "image/svg+xml";
+  configuredS3();
 });
