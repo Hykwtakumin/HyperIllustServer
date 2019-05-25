4a13030027cac2fc8e6c6e79cb78b0c82d872357
hykwtakumin <hykwtakumin@gmail.com>
2019-05-25 23:53:07 +0900

    view engine をhtmlに設定

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as cors from "cors";
+import * as path from "path";
 import * as multer from "multer";
 import { configuredS3 } from "./aws";
 
@@ -7,9 +8,16 @@ const app = express();
 app.listen(process.env.PORT || 3000);
 
 app.use(cors());
+console.log(__dirname);
+app.use(express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "/views"));
+//app.set("view engine", "html");
 
 app.get("/", (req, res) => {
-  res.json({ message: "hello!" });
+  //res.json({ message: "hello!" });
+  //res.render("index.html");
+  console.log(path.resolve(__dirname, "./views/index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.post("/upload/:hicId", (req, res) => {
