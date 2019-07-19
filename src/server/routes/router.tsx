026eedd821bc1b25794908f8fba3f289d624cb62
hykwtakumin <hykwtakumin@hykwtakuminnoMacBook-Pro.local>
2019-07-19 14:39:21 +0900

    socket.ioを実装中

--- src/server/routes/router.tsx
+++ src/server/routes/router.tsx
@@ -18,62 +18,93 @@ import * as dotenv from "dotenv";
 import * as moment from "moment";
 import * as fs from "fs";
 import { promisify } from "util";
-export const Router: express.Router = express.Router();
+import * as socketIo from "socket.io";
+import { publishUpdate } from "../services/socket";
 
-const uploader = multer({ dest: "tmp/" });
+export const Router = (io: socketIo.Server): express.Router => {
+  const router = express.Router();
 
-Router.get("/", (req: express.Request, res: express.Response) => {
-  res
-    .header("content-type", "text/html")
-    .send(renderToString(<BaseLayout title={"Hello"} />))
-    .end();
-});
-
-Router.get("/api/proxy/:url", (req: express.Request, res: express.Response) => {
-  const imageURL = decodeURIComponent(req.params.url);
-  fetchBase64
-    .remote(imageURL)
-    .then(data => {
-      res.send({ data: data[1] });
-    })
-    .catch(error => {
-      res.send(error);
-      throw error;
-    });
-});
-
-Router.post(
-  "/api/upload",
-  uploader.single("file"),
-  async (req: express.Request, res: express.Response) => {
-    const now = moment().format("YYYY-MM-DD-HH-mm-ss");
-    const fileName = `hyperillust_${now}_.svg`;
-    const mime: string = "image/svg+xml";
-
-    const rawData = await asyncReadFile(req.file.path);
-
-    try {
-      const result = await uploadFile(fileName, rawData, mime);
-      const url = result.Location;
-      asyncUnLink(req.file.path);
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.send(
-        JSON.stringify({
-          ok: true,
-          url: url
-        })
-      );
-    } catch (e) {
-      res.send(e);
+  const multerStorage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, "tmp/");
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
     }
-  }
-);
+  });
 
-Router.post(
-  "/api/postlayer",
-  uploader.single("file"),
-  async (req: express.Request, res: express.Response) => {
-    //const rawData = await asyncReadFile(req.file.path);
-    res.send({ layerPath: req.file.path });
-  }
-);
+  //const uploader = multer({ dest: "tmp/" });
+  const uploader = multer({ storage: multerStorage });
+
+  router.get("/", (req: express.Request, res: express.Response) => {
+    res
+      .header("content-type", "text/html")
+      .send(renderToString(<BaseLayout title={"Hello"} />))
+      .end();
+  });
+
+  router.get(
+    "/api/proxy/:url",
+    (req: express.Request, res: express.Response) => {
+      const imageURL = decodeURIComponent(req.params.url);
+      fetchBase64
+        .remote(imageURL)
+        .then(data => {
+          res.send({ data: data[1] });
+        })
+        .catch(error => {
+          res.send(error);
+          throw error;
+        });
+    }
+  );
+
+  router.post(
+    "/api/upload",
+    uploader.single("file"),
+    async (req: express.Request, res: express.Response) => {
+      const now = moment().format("YYYY-MM-DD-HH-mm-ss");
+      const fileName = `hyperillust_${now}_.svg`;
+      const mime: string = "image/svg+xml";
+
+      const rawData = await asyncReadFile(req.file.path);
+
+      try {
+        const result = await uploadFile(fileName, rawData, mime);
+        const url = result.Location;
+        asyncUnLink(req.file.path);
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.send(
+          JSON.stringify({
+            ok: true,
+            url: url
+          })
+        );
+      } catch (e) {
+        res.send(e);
+      }
+    }
+  );
+
+  router.post(
+    "/api/postlayer",
+    uploader.single("file"),
+    async (req: express.Request, res: express.Response) => {
+      const rawData = await asyncReadFile(req.file.path);
+      res.send({ layerPath: req.file.filename });
+      publishUpdate("scrapbox", io);
+    }
+  );
+
+  router.get(
+    "/api/getlayer/:name",
+    async (req: express.Request, res: express.Response) => {
+      const rawData = await asyncReadFile(`tmp/${req.params.name}`);
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Content-Type", "image/svg+xml");
+      res.send(rawData);
+    }
+  );
+
+  return router;
+};
--- src/server/services/socket.ts
+++ src/server/services/socket.ts
@@ -1,43 +1,44 @@
-// import { Server as HttpServer } from "http";
-// import * as socketIo from "socket.io";
-// import * as redis from "socket.io-redis";
-// import * as debug from "debug";
+import { Server as HttpServer } from "http";
+import * as socketIo from "socket.io";
+import * as redis from "socket.io-redis";
+import * as debug from "debug";
 
-// /*socketIOサーバーを立てる*/
-// function createSocketIOServer(server: HttpServer): socketIo.Server {
-//   const io = socketIo().listen(server);
-//   io.adapter(redis({ host: kRedisHost, port: parseInt(kRedisPort) }));
-//   return io;
-// }
+/*socketIOサーバーを立てる*/
+function createSocketIOServer(server: HttpServer): socketIo.Server {
+  const io = socketIo().listen(server);
+  //io.adapter(redis({ host: kRedisHost, port: parseInt(kRedisPort) }));
+  return io;
+}
 
-// const socketIOHandler = (io: socketIo.Server) => {
-//   io.on("connect", socket => {
-//     debug("user connected");
+const socketIOHandler = (io: socketIo.Server) => {
+  io.on("connect", socket => {
+    debug("user connected");
 
-//     /*問題IDが存在する場合*/
-//     if (socket.handshake.query["quizId"]) {
-//       const { quizId } = socket.handshake.query;
-//       debug(`roomID : ${quizId}`);
+    /*問題IDが存在する場合*/
+    if (socket.handshake.query["quizId"]) {
+      const { quizId } = socket.handshake.query;
+      debug(`roomID : ${quizId}`);
 
-//       socket.join(quizId, () => {
-//         let rooms = Object.keys(socket.rooms);
-//         debug(`rooms : ${rooms}`);
-//         io.to(quizId).emit(
-//           "lnq:message",
-//           `a new user has joined the room${quizId}`
-//         );
-//         socket.emit("lnq:message", `you joined room${quizId}`);
-//       });
+      socket.join(quizId, () => {
+        let rooms = Object.keys(socket.rooms);
+        debug(`rooms : ${rooms}`);
+        io.to(quizId).emit(
+          "hic:message",
+          `a new user has joined the room${quizId}`
+        );
+        socket.emit("hic:message", `you joined room${quizId}`);
+      });
 
-//       socket.on("disconnect", () => {
-//         socket.leave(quizId);
-//       });
-//     }
-//   });
-// };
+      socket.on("disconnect", () => {
+        socket.leave(quizId);
+      });
+    }
+  });
+};
 
-// const publishQuizUpdate = (id: string, io: socketIo.Server) => {
-//   io.to(id).emit("lnq:message", `quiz${id} updated!`);
-// };
+const publishUpdate = (id: string, io: socketIo.Server) => {
+  console.log(`layer ${id} updated!`);
+  io.to(id).emit("hic:message", `layer ${id} updated!`);
+};
 
-// export { createSocketIOServer, socketIOHandler, publishQuizUpdate };
+export { createSocketIOServer, socketIOHandler, publishUpdate };
