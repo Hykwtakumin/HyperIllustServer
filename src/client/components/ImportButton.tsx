46f51a0d3491ee9a928bf4e03b9baa50f15aab99
hykwtakumin <hykwtakumin@gmail.com>
2019-10-26 19:45:56 +0900

    - 画像インポート用コンポーネントを実装

--- src/client/components/ImportButton.tsx
+++ src/client/components/ImportButton.tsx
@@ -1,28 +1,71 @@
 import * as React from "react";
-import { FC } from "react";
+import { FC, useRef, useState } from "react";
 import { useModal, ButtonComponent } from "./share";
 import { PublishButtonProps } from "./PublishButton";
+import { restoreFromLocalStorage } from "./share/localStorage";
+import { HyperIllust } from "../../share/model";
 
 /*押すと他のハイパーイラストをインポートできるモーダルを召喚するボタン*/
 
 /*サムネはURLを渡すだけ*/
 /*なのでCORSをクリアしている必要がある*/
 
-export type HyperIllust = {
-  id: string;
-  title: string;
-  description: string;
-  thumbnail: string;
+export type ImportButtonProps = {
+  onSelected: (item: SelectedItem) => void;
 };
 
-export type ImportButtonProps = {
-  onSelected: (resourceURL: string) => void;
+export type SelectedItem = {
+  selsectedId: string;
 };
+
+//localに保存されているhyperIllustを全件取得する
+export function loadHyperIllusts(): Array<HyperIllust> {
+  const localHyperIllusts: HyperIllust[] = [];
+  if (localStorage) {
+    for (let key in localStorage) {
+      if (key && key.includes("hyperillust")) {
+        const restored = JSON.parse(localStorage.getItem(key)) as HyperIllust;
+        localHyperIllusts.push(restored);
+      }
+    }
+  }
+  return localHyperIllusts;
+}
 
 export const ImportButton: FC<ImportButtonProps> = (
   props: ImportButtonProps
 ) => {
   const { showModal } = useModal();
+  const [localIllust, setLocalIllust] = useState<HyperIllust[]>(
+    loadHyperIllusts()
+  );
+  const [selectedItem, setSelecteditem] = useState<string>("");
+  const selectedItemref = useRef<SelectedItem>(null);
+
+  const inner = (
+    <div className="ImportModalMenu">
+      {localIllust.map((item: HyperIllust, index: number) => {
+        <img
+          key={index}
+          className="ImportModalItem"
+          alt={item.id}
+          title={item.id}
+          src={item.sourceURL}
+          width={100}
+          height={80}
+          onClick={() => {
+            handleImageSelect(item.id);
+          }}
+        />;
+      })}
+    </div>
+  );
+
+  const handleImageSelect = (key: string) => {
+    setSelecteditem(key);
+    selectedItemref.current.selsectedId = selectedItem;
+    props.onSelected(selectedItemref.current);
+  };
 
   const popUpModal = () => {
     showModal({
@@ -31,12 +74,13 @@ export const ImportButton: FC<ImportButtonProps> = (
       content: (
         <>
           <div>
-            <h3>{`インポート`}</h3>
+            <h3>{`以下のリストからインポート`}</h3>
+            {inner}
           </div>
         </>
       ),
       onOk() {
-        props.onSelected("");
+        props.onSelected(selectedItemref.current);
       },
       onCancel() {},
       okText: "追加する",
