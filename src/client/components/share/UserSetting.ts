12491510470e9db18ff21de4e1653ebccc4383e6
hykwtakumin <hykwtakumin@penguin>
2019-11-06 17:11:22 +0900

    localStorageからUserInfoを読み込むようにした

---
+++ src/client/components/share/UserSetting.ts
@@ -0,0 +1,34 @@
+import { saveToLocalStorage, restoreFromLocalStorage } from "./localStorage";
+import { HyperIllustUser } from "../../../share/model";
+
+//いずれちゃんとしたUserをつくる
+//とりあえずで良い
+
+//一度設定したユーザー情報を復元する
+export function loadUserInfo(): HyperIllustUser {
+  if (window.localStorage) {
+    for (let key in localStorage) {
+      if (key && key.includes("hyperillust-user")) {
+        const restoredUser = JSON.parse(
+          localStorage.getItem(key)
+        ) as HyperIllustUser;
+        return restoredUser;
+      }
+    }
+  }
+}
+
+//まだUserが決まっていない状態(初回アクセス時)
+export async function setUserInfo() {
+  if (window.localStorage) {
+    const userName = location.href.split("/")[3];
+    const now = new Date();
+    const setUser: HyperIllustUser = {
+      id: userName,
+      name: userName,
+      createdAt: now,
+      updatedAt: now
+    };
+    await saveToLocalStorage<HyperIllustUser>(`hyperillust-user`, setUser);
+  }
+}
