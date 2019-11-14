755289fe0a2effcf386265104b100a71c25bbf51
hykwtakumin <hykwtakumin@gmail.com>
2019-11-14 09:55:20 +0900

    - リンク付き要素のスタイルを修正

--- src/client/components/Graphics/GroupDrawer.tsx
+++ src/client/components/Graphics/GroupDrawer.tsx
@@ -21,7 +21,7 @@ export const GroupDrawer: FC<GroupDrawerProps> = props => {
         return (
           <a id={group.id} key={index} href={group.href} target={"blank"}>
             <g transform={group.transform} pointerEvents={props.events}>
-              <StrokeDrawer strokes={group.strokes} events={props.events} />
+              <StrokeDrawer strokes={group.strokes} events={props.events} style={"linkedPath"} />
             </g>
           </a>
         );
