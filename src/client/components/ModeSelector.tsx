a2cf53676d36cce554540dbdee55e089dea5236a
hykwtakumin <hykwtakumin@gmail.com>
2019-11-21 02:19:45 +0900

    - アイコンや見た目を編集した

--- src/client/components/ModeSelector.tsx
+++ src/client/components/ModeSelector.tsx
@@ -14,10 +14,24 @@ export const ModeSelector: FC<ModeSelectorProps> = (
   return (
     <div style={{ padding: "3px" }}>
       <ButtonComponent
-        type={props.text === "draw" ? "default" : "primary"}
+        type={props.text === "draw" ? "orange" : "primary"}
         onClick={props.modeChange}
       >
-        {props.text}
+        {props.text === "draw" ? (
+          <img
+            src={"./icons/brush-24px.svg"}
+            alt={"描画モード"}
+            title={"描画モード"}
+            style={{ transform: "scale(1.5)" }}
+          />
+        ) : (
+          <img
+            src={"./icons/crop-24px.svg"}
+            alt={"編集モード"}
+            title={"編集モード"}
+            style={{ transform: "scale(1.5)" }}
+          />
+        )}
       </ButtonComponent>
     </div>
   );
